608374865d16cf9ed17c8cb7994cc68b82bdf9c1
Etta Schaye <ettaschaye@gmail.com>
2024-09-03 19:43:28 -0400

    adding exercise 7

--- d1_morning_exercise.R
+++ d1_morning_exercise.R
@@ -2,11 +2,16 @@
 #September 3rd, 2024
 #Quant Bio Bootcamp Day 1, Morning
 
-library("tidyverse")
-df <- read_tsv("~/Data/GTEx/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt")
+#Exercise 2
+library("tidyverse") #load tidyverse
+
+#Exercise 3
+df <- read_tsv("~/Data/GTEx/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt") #load text file and assign it to the variable df
 df <- df %>%
   mutate( SUBJECT=str_extract( SAMPID, "[^-]+-[^-]+" ), .before=1 )
 head(df) 
+
+#Exercise 4
 df %>%
   group_by(SUBJECT) %>%  #group by subject
   summarise(num_of_samples = n()) %>% #produce a new data frame that only includes grouping variables
@@ -22,6 +27,7 @@ df %>%
   arrange((num_of_samples)) 
 #GTEX-1JMI6 and GTEX-1PAR6 have the fewest samples, and both have 1 sample
 
+#Exercise 5
 df %>%
   group_by(SMTSD) %>%
   summarise(num_of_samples = n()) %>%
@@ -35,7 +41,7 @@ df %>%
 #Kidney-Medulla has the fewest number of samples, 4. 
 #Cervix-Ectocervix and Fallopian tube are tied for second fewest, with 9 samples each. 
 
-
+#Exercise 6
 df_npj8 <- subset(df, SUBJECT == "GTEX-NPJ8")
 
 df_npj8_dplyr <- df %>% 
@@ -51,5 +57,25 @@ View(df_npj8)
 
 df_npj8_Whole_Blood <- df_npj8 %>%
     filter(SMTSD == "Whole Blood")
+#Different technology was used to analyse different samples
 
-#I still need to complete the last task and will update the assignment after I do
+#Exercise 7
+df_autolysis <- df %>% #filter out NA values 
+      filter( !is.na(SMATSSCR) )
+
+df_autolysis <- df_autolysis %>% #create a new tibble using these operations
+  mutate() %>% #make a new column
+  group_by(SUBJECT) %>% #group by SUBJECT
+  summarise(mean_SMATSSCR = mean(SMATSSCR)) #calculate the mean SMATSSCR by subject
+
+df_autolysis %>% #filter to only those subjects with a SMATSSCR mean of 0
+    filter(mean_SMATSSCR == 0)
+
+ggplot(df_autolysis, 
+       aes(x = mean_SMATSSCR))+
+  geom_histogram()
+
+#It looks to me like the autolysis scores are a normal distribution centered at 1
+#I would probably display it as a histogram 
+
+
