e155c532ce379c2ed864c1ba57a4d4ddcea3e630
Etta Schaye <ettaschaye@gmail.com>
2024-09-11 18:10:03 -0400

    Fix violin plot and add interpretation

--- day4-lunch-exercise/day4-lunch-exercise.R
+++ day4-lunch-exercise/day4-lunch-exercise.R
@@ -12,8 +12,17 @@ expression <- expression %>%
 
 #Violin plot of expression data
 ggplot(data = expression,
-       mapping = aes(y = Tissue_Data, x = Log2_Expr)) +
+       mapping = aes(x = Tissue_Data, y = Log2_Expr)) +
   geom_violin() +
-  labs(x = "Log 2 Gene Expression", y = "Tissue + Gene") +
+  labs(y = "Log 2 Gene Expression", x = "Tissue + Gene") +
+  coord_flip()+
   theme_classic()
 
+#Given the tissue specificity and high expression level of these genes, are you surprised by the results?
+  #No, I'm not surprised. It makes sense that expression would be high in the tissues
+#What tissue-specific differences do you see in expression variability?
+  #Stomach, small intestine, and pituitary glands have a wide range of expression
+  #While pancreas tends to be narrowly highly expressed
+#Speculate on why certain tissues show low variability while others show much higher expression variability.
+  #The pancreas in general has high gene expression with low variability
+  #My guess is this is because the pancreas is fairly consistently expressing genes to produce enzymes
