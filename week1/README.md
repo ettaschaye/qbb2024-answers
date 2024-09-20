48ed5f93b14009ae524fd4f787d69b1b13538089
Etta Schaye <ettaschaye@gmail.com>
2024-09-19 23:06:08 -0400

    Updating all week 1 files

--- week1/README.md
+++ week1/README.md
@@ -19,6 +19,8 @@ desired_coverage = 3
 read_length = 100
 num_reads = int((genome_size * desired_coverage)/read_length)
 
+30,000 100 bp reads are required 
+
 ## Step 1.2
 ### Produce a 1D array (genome_coverage) with 1 million positions and a zero at every position
 genome_coverage = numpy.zeros(1000000, dtype= int)  
@@ -57,6 +59,11 @@ normal_estimates = scipy.stats.norm.pdf(xs, loc = 3, scale = math.sqrt(3))
 numpy.savetxt('poisson_results.csv', poisson_results, fmt='%.8e', delimiter=',')
 numpy.savetxt('normal_estimates.csv', normal_estimates, fmt='%.8e', delimiter=',')
 
+**In your simulation, how much of the genome has not been sequenced (has 0x coverage)?**
+50687 positions were not sequenced 
+**How well does this match Poisson expectations? How well does the normal distribution fit the data?**
+The Poisson estimate would be 49787 unsequenced nucleotides and the normal distribution predicts 51272 unsequenced nucleotides, so both distributions fit the simulated data fairly well on the low end. However, the normal distribution predicts higher coverage than what was simulated. 
+
 ## Step 1.5
 ### Repeat the simulation with 10X coverage 
 ### **How many 100bp reads are needed to sequence a 1Mbp genome to 10X coverage?**
@@ -64,6 +71,8 @@ genome_size = 1000000
 desired_coverage = 10
 read_length = 100
 num_reads = int((genome_size * desired_coverage)/read_length)
+
+100,000 100 bp reads are required 
 
 ### Produce a 1D array (genome_coverage_10X) with 1 million positions and a zero at every position
 genome_coverage_10X = numpy.zeros(1000000, dtype= int)  
@@ -102,16 +111,10 @@ normal_estimates_10X = scipy.stats.norm.pdf(xs_10X, loc = 10, scale = math.sqrt(
 numpy.savetxt('poisson_results_10X.csv', poisson_results, fmt='%.8e', delimiter=',')
 numpy.savetxt('normal_estimates_10X.csv', normal_estimates, fmt='%.8e', delimiter=',')
 
-
-
-
-
-
-
-
-
-
-
+**In your simulation, how much of the genome has not been sequenced (has 0x coverage)?**
+64 nucleotides have not been sequenced 
+**How well does this match Poisson expectations? How well does the normal distribution fit the data?**
+The Poisson estimate would be 45.4 unsequenced nucleotides and the normal distribution predicts 845 unsequenced nucleotides, so the Poisson estimate is fairly good and the normal distribution is a poorer predictor. The normal distribution overestimates coverage in general. 
 ## Step 1.6
 
 ### Repeat the simulation with 30X coverage 
@@ -121,8 +124,15 @@ desired_coverage = 30
 read_length = 100
 num_reads = int((genome_size * desired_coverage)/read_length)
 
+300,000 100 bp reads are required 
+
 ### Produce a 1D array (genome_coverage_30X) with 1 million positions and a zero at every position
-genome_coverage_30X = numpy.zeros(1000000, dtype= int)  
+genome_coverage_30X = numpy.zeros(1000000, dtype= int) 
+
+**In your simulation, how much of the genome has not been sequenced (has 0x coverage)?**
+4 positions were not sequenced 
+**How well does this match Poisson expectations? How well does the normal distribution fit the data?**
+The Poisson estimate would be 0.0000000936 unsequenced nucleotides and the normal distribution predicts 0.0214 unsequenced nucleotides, so both distributions underestimate the number of missed nucleotides (though not by much). In general, Poisson slightly underestimates the actual coverage, while the normal distribution overestimates it.  
 
 ### Simulate sequencing 30X coverage of a 1 millon bp genome
 **1. Use NumPy's pseudo-random number generator function to choose an integer between 0 and 999,900 to serve as our start position**
@@ -135,7 +145,7 @@ for i in range(300000):
     genome_coverage_30X[startpos:endpos] += 1
 
 **4. Save the coverage for each position in the genome as a text file**
-numpy.savetxt('genome_coverage_30X.csv', genome_coverage_10X, fmt='%d', delimiter=',')
+numpy.savetxt('genome_coverage_30X.csv', genome_coverage_30X, fmt='%d', delimiter=',')
 
 **5. Look within the genome coverage array and find the position with the highest value, which has been "sequenced" the most**
 maxcoverage_30X = numpy.max(genome_coverage_30X) 
@@ -151,7 +161,7 @@ poisson_results_30X = scipy.stats.poisson.pmf(xs_30, 10)
 
 **2. Calculate the normal distribution probability density function**
 
-normal_estimates_10X = scipy.stats.norm.pdf(xs_30, loc = 10, scale = math.sqrt(10))
+normal_estimates_30X = scipy.stats.norm.pdf(xs_30, loc = 10, scale = math.sqrt(10))
 
 **3. Save the results as text files**
 
@@ -198,3 +208,7 @@ dot -Tsvg edges.dot > ex2_digraph.svg
 
 **Here is one version where the maximum number of occurences for any 3-mer is 3**
 TTC TCT CTT TTA TAT ATT TTG TGA GAT ATT TTC TCA CAT ATT TTT 
+
+## Step 2.6
+**In a few sentences, what would it take to accurately reconstruct the sequence of the genome?**
+We would need to know how many times each 3-mer occurs. Just based on the de Brujin graph, this could be highly-repetitive genome of indeterminate length, or it could visit each node the minimum number of times required to solve the graph. My guess is that if you knew how many repeats there were of each k-mer, there are some de Brujin graphs that only have one solution, and some that have only a few solutions, but at minimum it would narrow the options down! 
--- week1/genome_assembly.R
+++ week1/genome_assembly.R
@@ -50,20 +50,148 @@ full_data_3X <- full_data_3X %>% mutate(poisson_predicted_n = probability_poisso
 full_data_3X <- full_data_3X %>% mutate(normal_predicted_n = probability_normal * 1000000)
 
 #Plot full data
-
-ggplot(full_data_3X) + #one bin for integers 0 to 13
+?pivot_longer
+ggplot(full_data_3X) +
   xlab("Simulated sequencing depth") +
   ylab("Number of nucleotides") +
   scale_x_discrete(limits = full_data_3X$depth, labels = full_data_3X$depth_label) +
-  theme_minimal() +
+  theme_bw() +
   geom_col(aes(x = depth, y = n), fill = "red", alpha = 0.5) +
   geom_col(aes(x = depth, y = poisson_predicted_n), fill = "blue", alpha = 0.5) +
-  geom_col(aes(x = depth, y = normal_predicted_n), fill = "yellow", alpha = 0.5)
+  geom_col(aes(x = depth, y = normal_predicted_n), fill = "gray", alpha = 0.5) +
+  annotate("text", x = 10, y = 100000, label = "Simulated distribution", color = "red") +
+  annotate("text", x = 10, y = 80000, label = "Poisson distribution", color = "blue") +
+  annotate("text", x = 10, y = 60000, label = "Normal distribution", color = "dark gray") +
+  labs(title = ("Simulated genome coverage 3X"))
 ggsave("ex1_*_cov.png")
 
 #Count how many times each coverage level occurred 
 print(full_data_3X)
-#50687 positions were not sequenced 
-#Poisson estimate for 0X: 0.04978707
-#With 1 million positions, 0X coverage should occur 49787.07 times
+
+
+### ### ### ### ### ### ### ### 
+### Repeat with 10X coverage ### 
+### ### ### ### ### ### ### ### 
+#Import simulated data
+  simulated_coverage_10X <- read_csv("genome_coverage_10X.csv", col_names = FALSE)
+simulated_coverage_10X <- simulated_coverage_10X %>% rename("position_coverage" = "X1")
+
+#Plot simulated data
+ggplot(simulated_coverage_10X, 
+       aes(position_coverage)) +
+  geom_histogram(bins = 28, alpha = 0.7) + #one bin for integers 0 to 27
+  xlab("Simulated sequencing depth") +
+  ylab("Number of nucleotides") +
+  theme_minimal()
+
+#Make a vector containing all coverage options (integers 0 to 13)
+sequencing_depth_10X <- 0:27 
+
+#Calculate Poisson distribution 
+poisson_overlay_10X <- dpois(x = sequencing_depth_10X, lambda = 10)
+
+#Calculate normal distribution
+normal_distribution_10X <- dnorm(x = sequencing_depth_10X, mean = 10, sd = 3.16 )
+
+#Make a tibble containing the range of coverage and the likelihood of an individual position having that coverage
+poisson_overlay_10X <- tibble(Column1 = sequencing_depth_10X, Column2 = poisson_overlay_10X)
+poisson_overlay_10X <- rename(poisson_overlay_10X, depth = Column1, probability_poisson = Column2)
+poisson_overlay_10X <- mutate(poisson_overlay_10X, depth_label = paste0(depth,"X"))
+
+normal_distribution_10X <- tibble(Column1 = sequencing_depth_10X, Column2 = normal_distribution_10X)
+normal_distribution_10X <- rename(normal_distribution_10X, depth = Column1, probability_normal = Column2)
+normal_distribution_10X <- mutate(normal_distribution_10X, depth_label = paste0(depth,"X"))
+
+
+#Count the number of positions with each coverage level 
+coverage_distribution_10X <- simulated_coverage_10X %>% count(position_coverage)
+
+#Make one tibble
+full_data_10X <- poisson_overlay_10X %>% left_join(coverage_distribution_10X, by = join_by(depth == position_coverage
+))
+full_data_10X <- full_data_10X %>% left_join(normal_distribution_10X)
+full_data_10X <- full_data_10X %>% mutate(poisson_predicted_n = probability_poisson * 1000000)
+full_data_10X <- full_data_10X %>% mutate(normal_predicted_n = probability_normal * 1000000)
+
+#Plot full data
+ggplot(full_data_10X) +
+  xlab("Simulated sequencing depth") +
+  ylab("Number of nucleotides") +
+  scale_x_discrete(limits = full_data_10X$depth, labels = full_data_10X$depth_label) +
+  theme_bw() +
+  geom_col(aes(x = depth, y = n), fill = "red", alpha = 0.5) +
+  geom_col(aes(x = depth, y = poisson_predicted_n), fill = "blue", alpha = 0.5) +
+  geom_col(aes(x = depth, y = normal_predicted_n), fill = "gray", alpha = 0.5) +
+  annotate("text", x = 21, y = 100000, label = "Simulated distribution", color = "red") +
+  annotate("text", x = 21, y = 80000, label = "Poisson distribution", color = "blue") +
+  annotate("text", x = 21, y = 60000, label = "Normal distribution", color = "dark gray") +
+  labs(title = ("Simulated genome coverage 10X"))
+  ggsave("ex1_10x_cov.png")
+
+#Count how many times each coverage level occurred 
+print(full_data_10X)
+
+
+### ### ### ### ### ### ### ### 
+### Repeat with 30X coverage ### 
+### ### ### ### ### ### ### ### 
+#Import simulated data
+simulated_coverage_30X <- read_csv("genome_coverage_30X.csv", col_names = FALSE)
+simulated_coverage_30X <- simulated_coverage_30X %>% rename("position_coverage" = "X1")
+
+#Plot simulated data
+ggplot(simulated_coverage_30X, 
+       aes(position_coverage)) +
+  geom_histogram(bins = 58, alpha = 0.7) + #one bin for integers 0 to 57
+  xlab("Simulated sequencing depth") +
+  ylab("Number of nucleotides") +
+  theme_minimal()
+
+#Make a vector containing all coverage options (integers 0 to 57)
+sequencing_depth_30X <- 0:57 
+
+#Calculate Poisson distribution 
+poisson_overlay_30X <- dpois(x = sequencing_depth_30X, lambda = 30)
+
+#Calculate normal distribution
+normal_distribution_30X <- dnorm(x = sequencing_depth_30X, mean = 30, sd = 5.47 )
+
+#Make a tibble containing the range of coverage and the likelihood of an individual position having that coverage
+poisson_overlay_30X <- tibble(Column1 = sequencing_depth_30X, Column2 = poisson_overlay_30X)
+poisson_overlay_30X <- rename(poisson_overlay_30X, depth = Column1, probability_poisson = Column2)
+poisson_overlay_30X <- mutate(poisson_overlay_30X, depth_label = paste0(depth,"X"))
+
+normal_distribution_30X <- tibble(Column1 = sequencing_depth_30X, Column2 = normal_distribution_30X)
+normal_distribution_30X <- rename(normal_distribution_30X, depth = Column1, probability_normal = Column2)
+normal_distribution_30X <- mutate(normal_distribution_30X, depth_label = paste0(depth,"X"))
+
+
+#Count the number of positions with each coverage level 
+coverage_distribution_30X <- simulated_coverage_30X %>% count(position_coverage)
+
+#Make one tibble
+full_data_30X <- poisson_overlay_30X %>% left_join(coverage_distribution_30X, by = join_by(depth == position_coverage
+))
+full_data_30X <- full_data_30X %>% left_join(normal_distribution_30X)
+full_data_30X <- full_data_30X %>% mutate(poisson_predicted_n = probability_poisson * 1000000)
+full_data_30X <- full_data_30X %>% mutate(normal_predicted_n = probability_normal * 1000000)
+
+#Plot full data
+ggplot(full_data_30X) +
+  xlab("Simulated sequencing depth") +
+  ylab("Number of nucleotides") +
+  scale_x_discrete(limits = full_data_30X$depth, labels = full_data_30X$depth_label) +
+  theme_bw() +
+  geom_col(aes(x = depth, y = n), fill = "red", alpha = 0.5) +
+  geom_col(aes(x = depth, y = poisson_predicted_n), fill = "blue", alpha = 0.5) +
+  geom_col(aes(x = depth, y = normal_predicted_n), fill = "gray", alpha = 0.5) +
+  annotate("text", x = 18, y = 100000, label = "Simulated distribution", color = "red") +
+  annotate("text", x = 18, y = 80000, label = "Poisson distribution", color = "blue") +
+  annotate("text", x = 18, y = 60000, label = "Normal distribution", color = "dark gray") +
+  labs(title = ("Simulated genome coverage 30X"))
+ggsave("ex1_30X_cov.png", scale = 2)
+
+#Count how many times each coverage level occurred 
+print(full_data_30X)
+
 
