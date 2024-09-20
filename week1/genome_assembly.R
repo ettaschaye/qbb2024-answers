c0df9cc1414dceb57a710494bfea10cd142f5892
Etta Schaye <ettaschaye@gmail.com>
2024-09-19 21:09:14 -0400

    Adding first plot

--- week1/genome_assembly.R
+++ week1/genome_assembly.R
@@ -9,38 +9,48 @@ setwd("qbb2024-answers/week1/")
 library(tidyverse)
 
 #Import simulated data
-genome_coverage <- read_csv("genome_coverage.csv", col_names = FALSE)
-
+simulated_coverage <- read_csv("genome_coverage.csv", col_names = FALSE)
+simulated_coverage <- simulated_coverage %>% rename("position_coverage" = "X1")
+              
 #Plot simulated data
-ggplot(genome_coverage, 
-       aes(X1)) +
+ggplot(simulated_coverage, 
+       aes(position_coverage)) +
   geom_histogram(bins = 14, alpha = 0.7) + #one bin for integers 0 to 13
   xlab("Simulated sequencing depth") +
   ylab("Number of nucleotides") +
   theme_minimal()
+
+#Make a vector containing all coverage options (integers 0 to 13)
 sequencing_depth <- 0:13 
+
+#Calculate Poisson distribution 
 poisson_overlay <- dpois(x = sequencing_depth, lambda = 3)
+
+#Make a tibble containing the range of coverage and the likelihood of an individual position having that coverage
 poisson_overlay <- tibble(Column1 = sequencing_depth, Column2 = poisson_overlay)
 poisson_overlay <- rename(poisson_overlay, depth = Column1, probability = Column2)
+poisson_overlay <- mutate(poisson_overlay, depth_label = paste0(depth,"X"))
 
 
-# Create the plot
-ggplot() +
-    geom_histogram(data = genome_coverage, aes(x = X1), bins = 14, alpha = 0.7) + #one bin for integers 0 to 13
-       xlab("Simulated sequencing depth") +
-       ylab("Number of nucleotides") +
-       theme_minimal() +
-  geom_col(data = poisson_overlay, aes(x = sequencing_depth, y = probability),fill = "blue", alpha = 0.7) +
-  labs(x = "Sequencing Depth", y = "Probability", title = "Title") + scale_y_continuous(sec.axis = sec_axis(~./max(table(genome_coverage$X1)), name = "Probability"))
-  theme_minimal() 
-  
-#I cannot get them to overlay successfully
+#Count the number of positions with each coverage level 
+coverage_distribution <- simulated_coverage %>% count(position_coverage)
+
+#Can I join these and make one tibble?
+full_data <- poisson_overlay %>% left_join(coverage_distribution, by = join_by(depth == position_coverage
+                                                    ))
+full_data <- full_data %>% mutate(poisson_predicted_n = probability * 1000000)
+
+ggplot(full_data) + #one bin for integers 0 to 13
+  xlab("Simulated sequencing depth") +
+  ylab("Number of nucleotides") +
+  scale_x_discrete(limits = full_data$depth, labels = full_data$depth_label) +
+  theme_minimal() +
+  geom_col(aes(x = depth, y = n), fill = "red", alpha = 0.5) +
+  geom_col(aes(x = depth, y = poisson_predicted_n), fill = "blue", alpha = 0.5)
+
 
 #Count how many times each coverage level occurred 
-genome_coverage
-coverage_tally <- genome_coverage %>% count(X1)
-coverage_tally
-
+print(full_data)
 #50687 positions were not sequenced 
 #Poisson estimate for 0X: 0.04978707
 #With 1 million positions, 0X coverage should occur 49787.07 times
